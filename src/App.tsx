d344f209cf2f76d1b0b38f9453a7a8c425b9ab65
david-matias <davidmatias8@gmail.com>
2022-10-08 20:16:37 -0300

    Estrutura inicial do projeto

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import * as C from './App.styles';
+
+const App = () => {
+  return (
+    <C.Conteiner>
+      <C.Header>
+        <C.HeaderText>Sistema Financeiro</C.HeaderText>  
+      </C.Header>
+      <C.Body>
+        ...
+      </C.Body>
+    </C.Conteiner>
+
+  );
+}
+
+export default App;
